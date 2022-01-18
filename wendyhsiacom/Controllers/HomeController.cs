9e622bfcf4cbac3ac21153bcf1e334441c0dfe32
Wendy <hsiaw@seattleu.edu>
2022-01-17 20:27:03 -0800

    added seattle children's page, finished up index page.

--- wendyhsiacom/Controllers/HomeController.cs
+++ wendyhsiacom/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace wendyhsiacom.Controllers
             return View();
         }
 
+        public IActionResult SeattleChristian2022()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
